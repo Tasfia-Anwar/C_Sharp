f435363edc61fa81934948f05797cbc325ca8a22
Tasfia-Anwar <tasfia1317@gmail.com>
2025-01-30 00:50:50 +0600

    Palindrome_Checking

---
+++ Lab_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class PlindromeChecker
+{
+    static void Main()
+    {
+    Console.Write("Enter a String: ");
+    string str = Console.ReadLine();
+
+    if(IsPalindrome(str))
+    {
+        Console.WriteLine($"'{str}' is a palindrome");
+    }
+    else 
+    {
+        Console.WriteLine($"'{str}' is not a palindrome");
+    }
+    }
+
+    static bool IsPalindrome(string str)
+    {
+        str = str.Replace(" ","").ToLower();
+
+        int left = 0;
+        int right = str.Length-1;
+
+        while(left<right)
+        {
+            if(str[left]!=str[right])return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
